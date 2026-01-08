e5e0661d281682d147e98c8e696d8637031529ed
peter <chuckaboxpeter@gmail.com>
2026-01-08 13:04:31 +1100

    feat: added board to screenstate

--- lib/wordle/views/wordle_screen.dart
+++ lib/wordle/views/wordle_screen.dart
@@ -11,6 +11,11 @@ class WordleScreen extends StatefulWidget {
 
 class _WordleScreenState extends State<WordleScreen> {
   GameStatus _gameStatus = GameStatus.playing;
+
+  final List<Word> _board = List.generate(
+    6,
+    (_) => Word(letters: List.generate(5, (_) => Letter.empty())),
+  );
   
   @override
   Widget build(BuildContext context) {
