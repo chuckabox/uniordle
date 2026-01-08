677e9279834ed5ece0e39bfeefac753f9ed4f6da
peter <chuckaboxpeter@gmail.com>
2026-01-08 12:14:45 +1100

    feat: added _WordleScreenState

---
+++ lib/wordle/views/wordle_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WordleScreen extends StatefulWidget {
+  const WordleScreen({Key? key}) : super(key: key);
+
+  @override
+  _WordleScreenState createState() => _WordleScreenState();
+}
+
+class _WordleScreenState extends State<WordleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
