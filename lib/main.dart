437c7c4101ce838da1a7ad009c9ff757e9f36db1
peter <chuckaboxpeter@gmail.com>
2026-01-11 14:43:38 +1100

    refactor: created assets folder and moved fonts into it

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_wordle/app/app.dart';
+// import 'package:window_size/window_size.dart';
 
 void main() { 
   runApp(const App()); 
