1bba818a23551252550293ce5b0d93acf79c7cf8
peter <chuckaboxpeter@gmail.com>
2026-01-11 14:57:12 +1100

    feat: added locked window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wordle/app/app.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 void main() { 
   runApp(const App()); 
+
+  
+  doWhenWindowReady(() {
+    appWindow.size = const Size(400, 600);
+    appWindow.minSize = const Size(400, 673);
+    appWindow.maxSize = const Size(400, 673);
+    appWindow.alignment = Alignment.center;
+    appWindow.show();
+  });
 }
