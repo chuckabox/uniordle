6c48f79528e22b05e6991d6b69ff054bde1aca1d
peter <chuckaboxpeter@gmail.com>
2026-01-15 16:23:10 +1100

    refactor: prevent instantiation by using appcolours for colouring

--- lib/app/app_colors.dart
+++ lib/app/app_colors.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
-const Color correctColor = Color(0xFF538D4E);
-const Color inWordColor = Color(0XFFDAC316);
-const Color notInWordColor = Color(0xFF3A3A3C);
-const Color tileBackground = Color(0xFF303436);
-const Color gameBackground = Color(0xFF0A0E17);
-const Color homeScreenBackground = gameBackground;
-const Color keyBackground = Color(0xFF707070);
+class AppColors {
+  AppColors._();
+
+  // Logic Colours (Game States)
+  static const Color correctColor = Color(0xFF538D4E);
+  static const Color inWordColor = Color(0XFFDAC316);
+  static const Color notInWordColor = Color(0xFF3A3A3C);
+
+  // Backgrounds
+  static const Color tileBackground = Color(0xFF303436);
+  static const Color gameBackground = Color(0xFF0A0E17);
+  static const Color homeScreenBackground = Color(0xFF0A0E17);
+  static const Color keyBackground = Color(0xFF707070);
+
+  // UI Elements
+
+  // Branding
+
+  // Game End States
+}
