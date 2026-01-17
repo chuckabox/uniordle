ea32e5e548e6937d6889aa211c83e4ee9e036140
peter <chuckaboxpeter@gmail.com>
2026-01-17 18:28:32 +1100

    feat: added icons and colours to disciplines

--- lib/features/home/widgets/discipline_icon.dart
+++ lib/features/home/widgets/discipline_icon.dart
@@ -20,12 +20,39 @@ class SubjectIcon extends StatelessWidget {
     );
   }
 
-  IconData _getIcon(String name) {
-    switch (name) {
-      case 'category':
-        return LucideIcons.layoutGrid;
-      default:
-        return LucideIcons.bookOpen;
-    }
+IconData _getIcon(String name) {
+  switch (name) {
+    case 'settings':
+      return LucideIcons.settings;
+    case 'code':
+      return LucideIcons.code;
+    case 'heart_pulse':
+      return LucideIcons.heartPulse;
+    case 'scale':
+      return LucideIcons.scale;
+    case 'brain':
+      return LucideIcons.brain;
+    case 'palette':
+      return LucideIcons.palette;
+    case 'briefcase':
+      return LucideIcons.briefcase;
+    case 'book_open':
+      return LucideIcons.bookOpen;
+    case 'graduation_cap':
+      return LucideIcons.graduationCap;
+    case 'calculator':
+      return LucideIcons.calculator;
+    case 'music':
+      return LucideIcons.music;
+    case 'flask_conical':
+      return LucideIcons.flaskConical;
+    case 'pen_tool':
+      return LucideIcons.penTool;
+    case 'building_2':
+      return LucideIcons.building2;
+    default:
+      return LucideIcons.bookOpen;
   }
+}
+
 }
